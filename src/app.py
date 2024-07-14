aa82c9753deee02df30242ef4385f421d4b1f498
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-07-14 18:25:51 -0500

    added AI response func, had openai api working

--- src/app.py
+++ src/app.py
@@ -1,16 +1,49 @@
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage, AIMessage 
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-# initialize chat_history
+# initialize chat_history stored in session_state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [] # a list to store HumanMessage objects
 
 st.set_page_config(page_title="Clean Energy AI Consultant", page_icon=":robot:")
 
 st.title("Clean Energy AI Consultant")
+
+
+
+# Get AI Response
+def get_ai_response(user_query, chat_history):
+    template = """
+    You are a professional consultant in the domain of clean energies. 
+    Please answer the following question by considering the the history of the conversation:
+
+    Chat history: {chat_history}
+
+    User question: {user_question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0,
+    )
+    
+    workflow_chain = prompt | llm | StrOutputParser()
+
+    response = workflow_chain.invoke({
+        "user_question": user_query,
+        "chat_history": chat_history
+    })
+
+    return response
+
 
 
 # Conversation Display between User and AI
@@ -35,7 +68,7 @@ if user_query is not None and user_query != "":
         st.markdown(user_query)
 
     with st.chat_message("AI"):
-        ai_response = "PLACEHOLDER"
+        ai_response = get_ai_response(user_query, st.session_state.chat_history)
         st.markdown(ai_response)
     
     st.session_state.chat_history.append(AIMessage(ai_response))
