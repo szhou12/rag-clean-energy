e3065806703616f3805c2509e5db3bb0a2d272ce
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-07-15 16:33:32 -0500

    added functionality to scrape content from a web url

--- README.md
+++ README.md
@@ -20,3 +20,7 @@ streamlit run src/app.py
 ### Langchain
 - [langchain_core](https://api.python.langchain.com/en/latest/core_api_reference.html)
 - [langchain_openai ChatOpenAI](https://api.python.langchain.com/en/latest/chat_models/langchain_openai.chat_models.base.ChatOpenAI.html)
+- [Streaming](https://python.langchain.com/v0.1/docs/expression_language/streaming/)
+
+### Tutorials
+- [Stream LLMs with LangChain + Streamlit | Tutorial](https://www.youtube.com/watch?v=zKGeRWjJlTU&t=240s&ab_channel=AlejandroAO-Software%26Ai)
--- src/app.py
+++ src/app.py
@@ -4,24 +4,33 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import WebBaseLoader
 
-load_dotenv()
+
+load_dotenv() # Load environment variables from .env file
 
 # initialize chat_history stored in session_state
 if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [] # a list to store HumanMessage objects
+    st.session_state.chat_history = [] # a list to store HumanMessage & AIMessage objects
 
-st.set_page_config(page_title="Clean Energy AI Consultant", page_icon=":robot:")
+st.set_page_config(page_title="Clean Energy AI Consultant", page_icon="\N{robot face}")
 
-st.title("Clean Energy AI Consultant")
+st.title("Clean Energy AI Consultant \N{robot face}")
 
 
+def get_doc_from_url(url):
+    '''
+    Scrape the website's content from the given URL
+    '''
+    loader = WebBaseLoader(url)
+    doc = loader.load()
+    return doc
 
 # Get AI Response
 def get_ai_response(user_query, chat_history):
     template = """
     You are a professional consultant in the domain of clean energies. 
-    Please answer the following question by considering the the history of the conversation:
+    Please answer the following question by considering the history of the conversation:
 
     Chat history: {chat_history}
 
@@ -30,7 +39,7 @@ def get_ai_response(user_query, chat_history):
 
     prompt = ChatPromptTemplate.from_template(template)
 
-    # If not explicitly specified, it will by default go to .env file and read the value from key "OPENAI_API_KEY" 
+    # If not explicitly specified, it will by default load the env var by the key "OPENAI_API_KEY" 
     llm = ChatOpenAI(
         model="gpt-3.5-turbo",
         temperature=0,
@@ -78,3 +87,12 @@ if user_query is not None and user_query != "":
        ai_response = st.write_stream(get_ai_response(user_query, st.session_state.chat_history))
     
     st.session_state.chat_history.append(AIMessage(ai_response))
+
+with st.sidebar:
+    st.header("Scrape URL")
+    website_url = st.text_input("Website URL")
+    button_pressed = st.button("Scrape")
+    if website_url is not None and website_url != "" and button_pressed:
+        doc = get_doc_from_url(website_url)
+        st.write(doc)
+            
